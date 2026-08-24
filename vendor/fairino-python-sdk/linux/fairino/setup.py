acd1df5d107f116b25bbf905d4efa333b8bd9986
zhj <zhj@users.noreply.github.com>
2026-08-24 19:41:03 +0800

    Remove SDK build artifacts from release

--- vendor/fairino-python-sdk/linux/fairino/setup.py
+++
@@ -1,6 +0,0 @@
-# setup.py
-# python3 setup.py build_ext --inplace
-# python setup.py build_ext --inplace
-from distutils.core import setup
-from Cython.Build import cythonize
-setup(name='Robot', ext_modules=cythonize('Robot.py'))
